92ebb3f9b87db1601d500aac76ae3044029a4ed4
tringuyen <tri.nh@hotmail.com>
2021-08-29 11:09:36 +0700

    upda

--- main.js
+++ main.js
@@ -122,7 +122,7 @@ app.post('/createorder/', function (req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-    pool.query('INSERT INTO user VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)', postData, function (error, results, fields) {
+    pool.query('INSERT INTO donhang VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)', postData, function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
