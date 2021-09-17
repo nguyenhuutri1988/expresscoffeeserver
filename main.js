06db61544825deb458a90dfd419f560c462a1354
tringuyen <tri.nh@hotmail.com>
2021-09-17 10:56:18 +0700

    up

--- main.js
+++ main.js
@@ -268,6 +268,20 @@ app.post('/getuserorder/', function (req, res) {
          res.end(JSON.stringify(results.rows));
      });
  });
+ 
+ 
+  app.put('/deletedonhang/', function (req, res) {
+     var postData = req.body;
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Credentials", true);
+     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+     pool.query('DELETE FROM donhang where madonhang=($1)', postData, function (error, results, fields) {
+         if (error) throw error;
+         res.end(JSON.stringify(results.rows));
+     });
+ });
 
 //rest api to authen
 // app.get('/admin', function (req, res) {
